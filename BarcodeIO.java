aee2da66d3a1d047bcf618f28e0d35ff1fd43b16
Nick Saunders <nicholas3000@sbcglobal.net>
2019-05-25 15:34:19 -0700

    Added BarcodeIO.java

---
+++ BarcodeIO.java
@@ -0,0 +1,82 @@
+/* Team 1
+ * Austin Ah Loo
+ * Ramon Lucindo
+ * Mikie Reed
+ * Mitchell Saunders
+ * Nick Saunders
+ * CST 338 - Module 4: Optical Barcode
+ */
+
+import java.util.*;
+
+/**
+ * Class that contains all parts of Lab 4 - Optical Barcode
+ *
+ * @author Team 1
+ */
+public class Assig4
+{
+   /**
+    * Starting point for program
+    *
+    * @param args the command line arguments
+    */
+   public static void main(String[] args)
+   { 
+      int i = 105;
+      String letter  =  Character.toString((char)i);
+      System.out.println(letter);
+   }
+   
+   public interface BarcodeIO
+   {
+      public boolean scan(BarcodeImage bc);
+      public boolean readText(String text);
+      public boolean generateImageFromText();
+      public boolean translateImageToText();
+      public void displayTextToConsole();
+      public void displayImageToConsole();
+   
+   }
+   
+   class BarcodeImage implements Cloneable
+   {
+      
+   }
+   
+   class DataMatrix implements BarcodeIO
+   {
+      public boolean scan( BarcodeImage bc )
+      {
+         
+      }
+      
+      public boolean readText( String text )
+      {
+         
+      }
+      
+      public boolean generateImageFromText()
+      {
+         
+      }
+      
+      public boolean translateImageToText()
+      {
+         
+      }
+      
+      public void displayTextToConsole()
+      {
+         
+      }
+      
+      void displayImageToConsole()
+      {
+         
+      }
+   }
+   
+   
+   
+}
