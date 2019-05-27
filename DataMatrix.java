2f235c5d125b31a6e28e56101882fab574e61ca3
Mitchell Saunders <d3thr3lic@gmail.com>
2019-05-26 23:18:48 -0700

    Fleshed out DataMatrix class more. Has not been tested yet, as BarcodeImage class needs to be funciontal first. Method cleanImage is wrong and needs to be reworked-see comments in method.

--- DataMatrix.java
+++ DataMatrix.java
@@ -14,8 +14,7 @@ class DataMatrix implements BarcodeIO
    
    public DataMatrix(BarcodeImage image)
    {
-      this.image = image;
-      //call scan();
+      scan(image);
    }
    
    public DataMatrix(String text)
@@ -25,8 +24,18 @@ class DataMatrix implements BarcodeIO
    
    public boolean scan(BarcodeImage bc)
    {
-      image = bc.clone();
-      cleanImage(); //call this at some point
+      try
+      {
+         image = bc.clone();
+      }
+      //Eclipse is giving me an error here, not sure what to do to fix it yet
+      catch (CloneNotSupportedException e)‏
+      {
+         
+      }
+      cleanImage();
+      this.actualWidth = computeSignalWidth();
+      this.actualHeight = computeSignalHeight();
       return true;
    }
    
@@ -38,6 +47,25 @@ class DataMatrix implements BarcodeIO
    
    public boolean generateImageFromText()
    {
+      //Supposed to be called after the text has been mutated via readText();
+      BarcodeImage newImage = new BarcodeImage();
+      //Assuming that text will be separated by \n, create an array of strings
+      String[] textArray = text.split("\n");
+      //Iterate through each text line, then each char in the line
+      //and if it is not whitespace, then set the pixel as true.
+      for (int row = 0; row < textArray.length; row++)
+      {
+         for (int column = 0; column < textArray[row].length(); column++)
+         {
+            if (textArray[row].charAt(column) != ' ')
+            {
+               newImage.setPixel(row, column, true);
+            }
+         }
+      }
+      //now that the image has been created from the text, it is out of place
+      //and needs to be shifted. Call cleanImage();
+      cleanImage();
       return true;
    }
    
@@ -58,12 +86,26 @@ class DataMatrix implements BarcodeIO
    
    private int computeSignalWidth()
    {
-      return 0;
+      //Assuming that image has been cleaned up, start at the bottom left corner
+      //and find the width of the image using the bottom solid line
+      int ctr = 0;
+      while (image.getPixel(BarcodeImage.MAX_WIDTH - 1, ctr) != false)
+      {
+         ctr++;
+      }
+      return --ctr;
    }
    
    private int computeSignalHeight()
    {
-      return 0;
+      //Assuming that image has been cleaned up, start at the bottom left corner
+      //and find the height of the image using the left solid line
+      int ctr = BarcodeImage.MAX_HEIGHT;
+      while (image.getPixel(ctr, 0) != false)
+      {
+         ctr++;
+      }
+      return --ctr;
    }
    
    public int getActualWidth()
@@ -78,8 +120,9 @@ class DataMatrix implements BarcodeIO
    
    private void cleanImage()
    {
-      BarcodeImage newImage = new BarcodeImage();
-      int bottomRowIndex, leftColumnIndex;
+      //this method is wrong. It needs to not look at the text, but look at 
+      //the image. This needs to be reworked.
+      int bottomRowIndex = 0, leftColumnIndex = 0;
       
       //make an iterateable text array that contains each row of characters
       String[] textArray = text.split("\n");
@@ -94,18 +137,31 @@ class DataMatrix implements BarcodeIO
             }
          }
       }
+      //Use helper method to do the shifting, then overwrite the instance of
+      //image with the cleaned version
+      this.image = shiftImageToCorner(image, bottomRowIndex, leftColumnIndex);
+   }
+   
+   private BarcodeImage shiftImageToCorner(BarcodeImage imageToShift,
+         int bottomRowOfValidImage, int leftMostColumnOfValidImage)
+   {
+      //Create new image with max size that is empty.
+      BarcodeImage newImage = new BarcodeImage();
+      //Find row/column offsets to map image to new image in correct location
+      int rowOffset = BarcodeImage.MAX_HEIGHT - bottomRowOfValidImage;
+      int columnOffset = leftMostColumnOfValidImage;
       
-      //now to shift the entire image to the bottom left
-      for (int row = textArray.length - 1; row >= 0; row--)
+      //Iterate through full-size empty image from bottom-to-top, left-to-right
+      //and use offset to map old image to lower left corner of new.
+      for (int row = BarcodeImage.MAX_HEIGHT - 1; row - rowOffset >= 0; row--)
       {
-         for (int column = 0; column < textArray.length - 1; column++)
+         for (int column = BarcodeImage.MAX_WIDTH - 1;
+               column - columnOffset >= 0; column--)
          {
-            if (this.image.getPixel(row, column) == true)
-            {
-               bottomRowIndex = row;
-               leftColumnIndex = column;
-            }
+            newImage.setPixel(row, column,
+                 imageToShift.getPixel(row - rowOffset, column - columnOffset));
          }
       }
+      return newImage;
    }
 }
