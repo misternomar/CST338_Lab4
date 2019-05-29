85b48a3ef2905554be3decea746c1497087aa790
misternomar <rlucindo@csumb.edu>
2019-05-28 19:24:57 -0700

    Update Assig4.java

--- Assig4.java
+++ Assig4.java
@@ -21,49 +21,49 @@ public class Assig4
     *
     * @param args the command line arguments
     */
-   public static void main(String[] args)
+   public static void main(String[] args) throws CloneNotSupportedException
    {
       String[] sImageIn =
-      {
-         "                                               ",
-         "                                               ",
-         "                                               ",
-         "     * * * * * * * * * * * * * * * * * * * * * ",
-         "     *                                       * ",
-         "     ****** **** ****** ******* ** *** *****   ",
-         "     *     *    ****************************** ",
-         "     * **    * *        **  *    * * *   *     ",
-         "     *   *    *  *****    *   * *   *  **  *** ",
-         "     *  **     * *** **   **  *    **  ***  *  ",
-         "     ***  * **   **  *   ****    *  *  ** * ** ",
-         "     *****  ***  *  * *   ** ** **  *   * *    ",
-         "     ***************************************** ",  
-         "                                               ",
-         "                                               ",
-         "                                               "
+         {
+            "                                               ",
+            "                                               ",
+            "                                               ",
+            "     * * * * * * * * * * * * * * * * * * * * * ",
+            "     *                                       * ",
+            "     ****** **** ****** ******* ** *** *****   ",
+            "     *     *    ****************************** ",
+            "     * **    * *        **  *    * * *   *     ",
+            "     *   *    *  *****    *   * *   *  **  *** ",
+            "     *  **     * *** **   **  *    **  ***  *  ",
+            "     ***  * **   **  *   ****    *  *  ** * ** ",
+            "     *****  ***  *  * *   ** ** **  *   * *    ",
+            "     ***************************************** ",  
+            "                                               ",
+            "                                               ",
+            "                                               "
 
-      };          
-      
-      String[] sImageIn_2 =
-      {
-            "                                          ",
-            "                                          ",
-            "* * * * * * * * * * * * * * * * * * *     ",
-            "*                                    *    ",
-            "**** *** **   ***** ****   *********      ",
-            "* ************ ************ **********    ",
-            "** *      *    *  * * *         * *       ",
-            "***   *  *           * **    *      **    ",
-            "* ** * *  *   * * * **  *   ***   ***     ",
-            "* *           **    *****  *   **   **    ",
-            "****  *  * *  * **  ** *   ** *  * *      ",
-            "**************************************    ",
-            "                                          ",
-            "                                          ",
-            "                                          ",
-            "                                          "
+         };          
+         
+         String[] sImageIn_2 =
+         {
+               "                                          ",
+               "                                          ",
+               "* * * * * * * * * * * * * * * * * * *     ",
+               "*                                    *    ",
+               "**** *** **   ***** ****   *********      ",
+               "* ************ ************ **********    ",
+               "** *      *    *  * * *         * *       ",
+               "***   *  *           * **    *      **    ",
+               "* ** * *  *   * * * **  *   ***   ***     ",
+               "* *           **    *****  *   **   **    ",
+               "****  *  * *  * **  ** *   ** *  * *      ",
+               "**************************************    ",
+               "                                          ",
+               "                                          ",
+               "                                          ",
+               "                                          "
 
-      };
+         };
      
       BarcodeImage bc = new BarcodeImage(sImageIn);
       DataMatrix dm = new DataMatrix(bc);
@@ -127,54 +127,103 @@ public class Assig4
        * converts it to the internal 2D 
        * array of booleans.
        */      
-      public BarcodeImage(String[] str_data)
+      public BarcodeImage(String[] data)
       {
+         int row, col, displayRow;
+         
+         //instantiate the data and set all to false
          image_data = new boolean[MAX_HEIGHT][MAX_WIDTH];
-         for (int row = 0; row < str_data.length - 1; row++)
+         for (row = 0; row < image_data.length; row++)
          {
-            for (int column = 0; column < str_data[row].length() - 1; column++)
+            for (col = 0; col < image_data[row].length; col++)
+               image_data[row][col] = false;
+         }
+         
+         //sanity check the length of the data, if ok populate the array
+         if (checkSize(data))
+         {
+            for (row = 0, displayRow = image_data.length - data.length; 
+                  displayRow < image_data.length; row++, displayRow++)
             {
-               boolean setTrue = (str_data[row].charAt(column) == '*');
-               this.setPixel(row, column, setTrue);
+               for (col = 0; col < data[row].length(); col++)
+               {
+                  if (data[row].charAt(col) == DataMatrix.WHITE_CHAR)
+                     setPixel(displayRow, col, false);
+                  else
+                     setPixel(displayRow, col, true);
+               }
             }
          }
       }
 
-      public boolean setPixel(int row, int col, boolean value)
+      public void displayToConsole()
       {
-         if (checkSize(row, col))
+         int row, col;
+         char temp;
+         
+         // this method takes much from the TwoDimImage class
+         //top border first
+         System.out.println();
+         for ( col = 0; col < MAX_WIDTH + 2; col++ )
+            System.out.print("-");
+         System.out.println();
+         
+         // then fill in the middle
+         for (row = 0; row < MAX_HEIGHT; row++)
          {
-            this.image_data[row][col] = value;
-            return true;
+            System.out.print("|");
+            for (col = 0; col < MAX_WIDTH; col++)
+            {
+               temp = DataMatrix.boolToChar(image_data[row][col]);
+               System.out.print(temp);
+            }
+            System.out.println("|");
          }
-         return false;
+         
+         // bottom
+         for ( col = 0; col < MAX_WIDTH + 2; col++ )
+            System.out.print("-");
+         System.out.println();
       }
       
       public boolean getPixel(int row, int col)
       {
-         if (checkSize(row, col))
+         try 
          {
-            return this.image_data[row][col];
+            return image_data[row][col];   
          }
-         return false;
+         catch (ArrayIndexOutOfBoundsException e)
+         {
+            return false;
+         }
       }
-     
-      private boolean checkSize(int row, int col)
+      
+      public boolean setPixel(int row, int col, boolean value)
       {
-         //Checks for bounds of rows and col being within Max height/width
-         return (row < MAX_HEIGHT && row >= 0 && col < MAX_WIDTH && col >= 0 );
+         try 
+         {
+            image_data[row][col] = value;
+         }
+         catch (ArrayIndexOutOfBoundsException e)
+         {
+            return false;
+         }
+         return true;
       }
-
-      /*
-       * Will checkSize be used here?
-       * 
-       * 
-       */
+      
       private boolean checkSize(String[] data)
       {
-         return false;
+         //important method to make sure the string is not too long
+         if (data == null || data.length > MAX_HEIGHT)
+            return false;
+         for (int k = 0; k < data.length; k++)
+         {
+            if (data[k] == null || data[k].length() > MAX_WIDTH)
+               return false;
+         }
+         return true;
       }
-
+      
       public BarcodeImage clone()
       {  
          BarcodeImage barcodeImage = new BarcodeImage();
